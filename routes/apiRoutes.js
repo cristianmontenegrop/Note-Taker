425363a8eaacd0bc6873b5518046bfbd911a3e30
Cris M <cristian.montenegro.p@gmail.com>
2020-05-17 01:21:02 -0700

    server is running, htmlroutes ready

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,28 +4,28 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
+var notesData = require("../data/db");
+// var waitListData = require("../data/waitinglistData");
 
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
+  app.get("/api/notes", function (req, res) {
+    res.json(notesData);
   });
 
-  app.get("/api/waitlist", function(req, res) {
-    res.json(waitListData);
-  });
+  // app.get("/api/waitlist", function(req, res) {
+  //   res.json(waitListData);
+  // });
 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
@@ -35,29 +35,31 @@ module.exports = function(app) {
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/tables", function(req, res) {
+  app.post("/api/notes", function (req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
+    // if (tableData.length < 5) {
+    notesData.push(req.body);
+    res.json(true);
+    // }
+    // else {
+    // waitListData.push(req.body);
+    // res.json(false);
+    // }
   });
 
   // ---------------------------------------------------------------------------
   // I added this below code so you could clear out the table while working with the functionality.
   // Don"t worry about it!
 
-  app.post("/api/clear", function(req, res) {
+  app.post("/api/clear", function (req, res) {
     // Empty out the arrays of data
     tableData.length = 0;
     waitListData.length = 0;
 
-    res.json({ ok: true });
+    res.json({
+      ok: true
+    });
   });
 };
