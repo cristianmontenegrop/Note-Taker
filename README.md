cf34a49aea9c633bb1b7f77eacf1553894c1a674
Cris M <cristian.montenegro.p@gmail.com>
2020-05-17 06:23:42 -0700

    Updating readme file and cleaning files

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# H11-Note-Taker
+
+                    
+# Note Taker
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/cristianmontenegrop/H11-Note-Taker)
+
+
+## Description
+
+This application enables a user that needs to keep track of a lot of information, being able to take persistent notes that allows users to have written information available when needed. The application lets users create, save, delete and view previously saved notes. 
+
+[A link to the deployed Applicaction can be found here](https://h11-note-taker.herokuapp.com/)
+
+
+## Table of Contents 
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Questions](#questions)
+
+
+## Usage
+
+Simply access the application here https://h11-note-taker.herokuapp.com/
+
+## License
+
+This project is licensed under the MIT license.
+  
+## Contributing
+
+Reach out through Slack, ID: UTW6F607J               
+
+## Questions
+
+IF you need further assistance contact me at cristian.montenegro.p@gmail.com
+
+
+
+                    
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -28,36 +28,17 @@ module.exports = function (app) {
   //   res.json(waitListData);
   // });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
 
   app.post("/api/notes", function (req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-    // if (tableData.length < 5) {
+
     newNote = req.body;
     newNote.id = notesData.length + 1;
     notesData.push(newNote);
     console.log(newNote);
     res.json(true);
     console.log("post method executed")
-
-    // }
-    // else {
-    // waitListData.push(req.body);
-    // res.json(false);
-    // }
   });
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
 
   app.delete("/api/notes/:id", function (req, res) {
 
