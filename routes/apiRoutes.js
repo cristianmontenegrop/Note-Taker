9b3568140202fb2c22ca6183c8d479b900b1058c
Cris M <cristian.montenegro.p@gmail.com>
2020-05-17 06:26:41 -0700

    cleaning missing file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,32 +1,13 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
 var notesData = require("../data/db.json");
-// var waitListData = require("../data/waitinglistData");
 
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
 
 module.exports = function (app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
 
   app.get("/api/notes", function (req, res) {
     res.json(notesData);
     console.log("get method executed")
   });
 
-  // app.get("/api/waitlist", function(req, res) {
-  //   res.json(waitListData);
-  // });
 
 
   app.post("/api/notes", function (req, res) {
